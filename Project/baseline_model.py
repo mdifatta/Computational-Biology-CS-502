3a6fcef97cb4e5c339af53b17d7349d48569c916
flavio.dipalo <flavio.dipalo@gmail.com>
2018-11-20 00:27:56 -0600

    code refactor

--- Project/baseline_model.py
+++ Project/baseline_model.py
@@ -1,7 +1,7 @@
 # This Python 3 environment comes with many helpful analytics libraries installed
 # It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
 # For example, here's several helpful packages to load in 
-from Project.telegram_bot import TelegramBot
+from telegram_bot.telegram_bot import TelegramBot
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
